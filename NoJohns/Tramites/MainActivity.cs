2f44cfdebbc6bf3ce33ce0e095779b70c6219128
memopeniche <memo.peniche93@gmail.com>
2015-05-27 15:34:43 -0500

    app 0.1 Signed-off-by: memopeniche <memo.peniche93@gmail.com>

---
+++ NoJohns/Tramites/MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using RestSharp;
+
+
+namespace Tramites
+{
+	
+	[Activity (Label = "Tramites", MainLauncher = true, Icon = "@drawable/logo")]
+	public class MainActivity : Activity
+	{
+		public class Login{
+			public string User { get;set;}
+			public string Password { get; set;}
+			public string Comment{ get; set;}
+		}
+		/*public override void OnBackPressed()
+//		{
+			long doublePressInterval_ms = 3000;
+			DateTime lastPressTime = DateTime.Now;
+			DateTime pressTime = DateTime.Now;
+			if ((pressTime - lastPressTime).TotalMilliseconds <= doublePressInterval_ms) 
+			{
+				Java.Lang.JavaSystem.Exit(0);
+			}
+		}*/
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.Login);
+			EditText pass = FindViewById<EditText> (Resource.Id.Pass);
+			EditText user = FindViewById<EditText>(Resource.Id.User);
+			ImageView logo = FindViewById<ImageView> (Resource.Id.Logo);
+			user.Text = null;
+
+			button.Click += delegate 
+			{
+				/*
+				var client= new RestClient ("http://nojohns-api.azurewebsites.net/");
+				var request= new RestRequest("api/clients/1",Method.GET);
+				request.RequestFormat = DataFormat.Json; 
+				var response = client.Execute<Login> (request);
+				user.Text= response.Data.Comment;
+				*/
+				var intent = new Intent(this, typeof(Profile));
+				StartActivity(intent);
+				Finish();
+			};
+		}
+	}
+}
+
+
---
+++ NoJohns/Tramites/ProfileActivity.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RestSharp;
+
+namespace Tramites
+{
+	[Activity (Label = "Profile",Icon = "@drawable/logo")]			
+	public class Profile : Activity
+	{
+		public class jsonProfile{
+			public string User { get;set;}
+			public string Password { get; set;}
+			public string Mail{ get; set;}
+			public string fName{ get; set;}
+			public string lName{ get; set;}
+			public string Address{ get; set;}
+			public string Phone{ get; set;}
+		}
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Profile);
+
+			TextView plName = FindViewById<TextView> (Resource.Id.plName);
+			TextView pAddress = FindViewById<TextView> (Resource.Id.pAddress);
+			TextView pPhone = FindViewById<TextView> (Resource.Id.pPhone);
+			TextView pMail = FindViewById<TextView> (Resource.Id.pMail);
+			ImageView MailIcon = FindViewById<ImageView> (Resource.Id.MailIcon);
+			ImageView AddressIcon = FindViewById<ImageView> (Resource.Id.AddresIcon);
+			ImageView PhoneIcon = FindViewById<ImageView> (Resource.Id.PhoneIcon);
+			// Create your application here
+
+			var client= new RestClient ("http://nojohns-api.azurewebsites.net/");
+			var request= new RestRequest("api/clients/1",Method.GET);
+			request.RequestFormat = DataFormat.Json; 
+			var response = client.Execute<jsonProfile> (request);
+			plName.Text = response.Data.fName +" " + response.Data.lName;
+			pAddress.Text = response.Data.Address;
+			pPhone.Text = response.Data.Phone;
+			pMail.Text = response.Data.Mail;
+
+		}
+	}
+}
+
