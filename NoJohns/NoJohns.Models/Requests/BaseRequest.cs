d96d46fe299dde976984b2483444b72f3271d29f
eduardinio333 <eduardinio33340@gmail.com>
2015-06-01 18:07:00 -0500

    Modlels Signed-off-by: eduardinio333 <eduardinio33340@gmail.com>

---
+++ NoJohns/NoJohns.Models/Requests/BaseRequest.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoJohns.Models.Requests
+{
+    public abstract class BaseRequest
+    {
+        public string ToRequestString()
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(this));
+            return System.Convert.ToBase64String(plainTextBytes);            
+        }
+
+        public static T ToRequest<T>(string request)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(request);
+            var text = System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+            return JsonConvert.DeserializeObject<T>(text);
+        }
+    }
+
+    public abstract class BaseRequest<T> : BaseRequest
+    {        
+        public abstract IEnumerable<T> FilterRequest(IEnumerable<T> enumerable);
+    }
+}
---
+++ NoJohns/NoJohns.Models/Requests/CommentRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoJohns.Models.Requests
+{
+
+    class CommentRequest : BaseRequest <Comments>
+    {
+        public int? ClientId { get; set; }
+        public string Comment { get; set; }
+        public bool ClientType { get; set; }
+        public override IEnumerable<Comments> FilterRequest(IEnumerable<Comments> enumerable)
+        {
+            if (ClientId.HasValue)
+                enumerable = enumerable.Where(client => client.Id == ClientId);
+            if (!String.IsNullOrWhiteSpace(Comment))
+                enumerable = enumerable.Where(client => String.Equals(client.Comment, Comment, StringComparison.InvariantCultureIgnoreCase));
+            return enumerable;
+        }
+    }
+}
